283d83da5058529ecca4e147d3dc912abf0668e6
sandeepanainala <nainala_sandeep@yahoo.com>
2024-05-22 16:20:06 +0530

    vpc

---
+++ variables.tf
@@ -0,0 +1,102 @@
+##### project ####
+variable "project_name" {
+  type = "string"
+}
+
+variable "environment" {
+  type = sting
+  default = "dev"
+}
+
+variable "common_tags" {
+  type = map
+}
+
+
+#### VPC ####
+variable "vpc_cidr" {
+  type = string
+  default = "10.0.0.0/16"
+}
+
+variable "enable_dns_hostnames" {
+  type = bool
+}
+
+variable "vpc_tags" {
+  type = map
+  default = {}
+}
+
+### IGW
+variable "igw_tags" {
+  type = map
+  default = {}
+}
+
+### public subnet variables
+variable "public_subnet_cidrs" {
+  type = list
+  validation {
+    condition = length(var.public_subnet_cidrs) == 2
+    error_message = "Please provide 2 valid public subnet CIDRS"
+  }
+}
+
+variable "public_subnet_cidr_tags" {
+  type = map
+  default = {}
+}
+
+### private subnet variables
+variable "private_subnet_cidrs" {
+  type = list
+  validation {
+    condition = length(var.private_subnet_cidrs)  == 2
+    error_message = "PLease provide 2 valid private subnet CIDRS"
+  }
+}
+
+variable "private_subnet_cidrs_tags" {
+  type = map
+  default = {}
+}
+
+### database subnet variables
+
+variable "database_subnet_cidrs" {
+  type = list
+  validation {
+    condition = length(var.database_subnet_cidrs) == 2
+    error_message = "Please provide 2 valid database subnet CIDRS"
+  }
+}
+
+variable "database_subnet_cidrs_tags" {
+  type = map
+  default = {}
+}
+
+### NAT gateway tags ###
+variable "nat_gateway_tags" {
+  type = map
+  default = {}
+}
+
+### public route table ###
+variable "public_route_table_tags" {
+  type = map
+  default = {}
+}
+
+### private route table ##
+variable "private_route_table_tags" {
+  type = map
+  default = {}
+}
+
+### database route table ##
+variable "database_route_table_tags" {
+  type = map
+  default = {}
+}
