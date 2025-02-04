517bbd3c89304e37b89e7903f599e20d5fdfedcf
sandeepanainala <nainala_sandeep@yahoo.com>
2025-02-04 12:58:41 +0530

    Terraform - creating vpc module

--- vpc.tf
+++ vpc.tf
@@ -28,6 +28,8 @@ resource "aws_internet_gateway" "gw" {
 ### Public Subnets ###
 resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidrs)  # Create one public subnet per public subnet CIDR block in the list of public_subnet_cidrs
+  availability_zone = local.az_names[count.index]
+  map_public_ip_on_launch = true
   vpc_id = aws_vpc.main.id
   cidr_block = var.public_subnet_cidrs[count.index]  # Use the CIDR block from the list of public_subnet_cidrs for this subnet creation iteration
 
@@ -35,7 +37,7 @@ resource "aws_subnet" "public" {
     var.common_tags,
     var.public_subnet_cidrs_tags,
     {
-      Name = local.az_names[count.index] # Use the availability zone name from the list of az_names for this subnet creation iteration
+      Name = "${local.resource_name}-${local.az_names[count.index]}" # Use the AZ name from the list of az_names for this subnet creation iteration to create a unique name for this subnet resource instance
     }
   )
 }
