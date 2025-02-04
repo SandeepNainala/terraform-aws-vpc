05f0a991773a49340ec018f90451f7ca81c3f0d2
sandeepanainala <nainala_sandeep@yahoo.com>
2025-02-04 16:15:03 +0530

    Terraform - creating vpc module

--- vpc.tf
+++ vpc.tf
@@ -130,3 +130,32 @@ resource "aws_route_table" "database" {
     }
   )
 }
+
+resource "aws_route" "public_route" {
+  route_table_id = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.gw.id
+}
+
+resource "aws_route" "private_route_nat" {
+  route_table_id = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.nat.id
+}
+
+resource "aws_route" "database_route_nat" {
+  route_table_id = aws_route_table.database.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.nat.id
+}
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidrs)     # Create one association per public subnet CIDR block in the list of public_subnet_cidrs
+  route_table_id = aws_route_table.public.id
+  subnet_id = aws_subnet.public[*].id        # * is a wildcard that tells Terraform to use all of the public subnet IDs here we have 2 public subnets
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnet_cidrs)     # Create one association per private subnet CIDR block in the list of private_subnet_cidrs
+  route_table_id = aws_route_table.private.id
+  subnet_id = aws_subnet.private[*].id        # * is a wildcard that tells Terraform to use all of the public subnet IDs here we have 2 private subnets
+}
