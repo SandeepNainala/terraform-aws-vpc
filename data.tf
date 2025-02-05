31b524e59249c3e00000d8ed0fddbb1f47ad025e
sandeepanainala <nainala_sandeep@yahoo.com>
2025-02-05 12:37:17 +0530

    Terraform - creating vpc module

--- data.tf
+++ data.tf
@@ -2,6 +2,10 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 /*data "aws_vpc" "default" {
   default = true
 }
--- peering.tf
+++ peering.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc_peering_connection" "peering" {
   count = var.is_peering_required ? 1 : 0
   vpc_id      = aws_vpc.main.id      ## VPC ID of the requester VPC
-  peer_vpc_id = var.acceptor_vpc_id == "" ? "default-vpc" : var.acceptor_vpc_id   ## VPC ID of the accepter VPC (default VPC)
+  peer_vpc_id = var.acceptor_vpc_id == "" ? data.aws_vpc.default.id : var.acceptor_vpc_id   ## VPC ID of the accepter VPC (default VPC)
 }
