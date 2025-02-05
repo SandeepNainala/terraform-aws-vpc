ce76a65505e1239f72a540b1319dd198ee211931
sandeepanainala <nainala_sandeep@yahoo.com>
2025-02-05 15:43:20 +0530

    Terraform - creating vpc module

--- peering.tf
+++ peering.tf
@@ -40,3 +40,10 @@ resource "aws_route" "database_peering" {
   vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id  # VPC peering connection ID
 }
 
+resource "aws_route" "default_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id = data.aws_route_table.main.id     # default VPC route table
+
+  destination_cidr_block = data.aws_vpc.default.cidr_block   ## CIDR block of the acceptor VPC (default VPC)
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id  # VPC peering connection ID
+}
